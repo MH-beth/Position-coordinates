744118353b201f02e7836236881740cea858c552
MH15O <76598767+MH15O@users.noreply.github.com>
2021-01-06 16:17:18 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function loc(){
+    // select query 
+    let lati = document.getElementById('latitude');
+    let longi = document.getElementById('longitude');
+    let accur = document.getElementById('accuracy');
+    let er = document.getElementById('error');
+
+    let options = {
+        enableHighAccuracy : true,
+        timeOut:5000,
+        maximumAge:0
+    }
+    function sucess (pos){
+        crd = pos.coords;
+        lati.textContent = `Latitude : ${crd.latitude}`;
+        longi.textContent = `Longitude :${crd.longitude}`;
+        accur.textContent = `Accuracy: ${Math.trunc(crd.accuracy)/1000} km`;
+    }
+    function error(err){
+        er.textContent = `ERROR(${err.code}): ${err.message}`
+    }
+    navigator.geolocation.getCurrentPosition(sucess, error, options);
+}
